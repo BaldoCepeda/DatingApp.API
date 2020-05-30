172fb475281435ce33fbcdd5ab963497cf66fe45
Baldo Cepeda Macias <baldocepeda@gmail.com>
2020-05-30 15:49:21 -0500

    first commit

---
+++ Data/DataContext.cs
@@ -0,0 +1,11 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+        public DbSet<Value> Values { get; set; } //The name of this property is the name of the table in the DB
+    }
+}
